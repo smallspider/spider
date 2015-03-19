6463ed3876c0fb6ca7820851921020f4cba99302
yangguangftlp <yangguangftlp@163.com>
2015-03-19 16:12:12 +0800

    提交代码

---
+++ modules/spider-msg-engine/src/main/java/org/spider/msg/engine/MessageEngine.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.spider.msg.engine;
+
+/**
+ * @author yangguangftlp
+ *
+ * 2015年3月19日
+ */
+public class MessageEngine {
+
+}
---
+++ modules/spider-msg-engine/src/main/java/org/spider/msg/engine/MessageEngineConfig.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.spider.msg.engine;
+
+/**
+ * 消息引擎配置
+ * 
+ * @author yangguangftlp
+ * 
+ *         2015年3月19日
+ */
+public class MessageEngineConfig {
+
+}
